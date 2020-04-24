8f447c14f31d0332cd727283050789366c7517bc
Student <student@Students-MacBook-Air.local>
2020-04-24 13:49:48 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import Card from './Components/Card';
+
 function App() {
   return (
+    <div>
     <div className="App">
       <header className="App-header">
        Store Name Goes Here
       </header>
-      <h1>My Listings</h1>
+        <h1>My Listings</h1>
+        </div>
+        
+        <Card/>
     </div>
   );
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Components/Styles/Card.css';
+
+class Card extends React.Component{
+    render(){
+        return(<div>
+            hello from card
+            <button>+</button>
+        </div>);
+    }
+}
+
+export default Card;
