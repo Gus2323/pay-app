5234cf2bc07d7ea465cbfe2f8eb3298a94ac38e0
Santos Castaneda <santoscastaneda@Gs-iMac.local>
2020-04-24 15:55:38 -0700

    owner style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+//Styles
+import "./App.css";
 
 //Components
-import Card from './Components/Card';
+import AddItem from "./Components/AddItem";
 
 function App() {
   return (
-    <div>
     <div className="App">
-      <header className="App-header">
-       Store Name Goes Here
-      </header>
-        <h1>My Listings</h1>
-        </div>
-        
-        <Card/>
+      <header className="App-header">Store Name Goes Here</header>
+      <h1>My Listings</h1>
+      <div className="card-container">
+        <AddItem />
+      </div>
     </div>
   );
 }
---
+++ src/Components/AddItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Components/Styles/AddItem.css";
+import "../App.css";
+
+class AddItem extends React.Component {
+  render() {
+    function handleClick() {
+      return console.log("clicked");
+    }
+    return (
+      <div className="card">
+        <h1 className="new">Add New Item</h1>
+        <button onClick={handleClick}>+</button>
+      </div>
+    );
+  }
+}
+
+export default AddItem;
